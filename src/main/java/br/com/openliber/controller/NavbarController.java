b8386584fd1142774368735f096f5ddd2c46b932
vinezof2 <joaovitorvinezof@gmail.com>
2019-06-03 09:50:50 -0300

    novas páginas

--- src/main/java/br/com/openliber/controller/NavbarController.java
+++ src/main/java/br/com/openliber/controller/NavbarController.java
@@ -58,7 +58,57 @@ public class NavbarController {
 	}
 	
 	@GetMapping("/livro/novo")
-	public String novoLivro() {
-		return "/upload";
+	public String novoLivro(Model model) {
+		model.addAttribute("capa", "/imagem/cover_livro/placeholder.jpg");
+		model.addAttribute("selecionado", "");
+		return "/livro-form";
+	}
+	
+	@GetMapping("/livro/editar")
+	public String editarLivro(Model model) {
+		model.addAttribute("capa", "/imagem/cover_livro/eloquent_javascript.png");
+		model.addAttribute("titulo", "Eloquent Javascript");
+		model.addAttribute("autor", "Marijn Haverbeke");
+		model.addAttribute("ano", "2017");
+		model.addAttribute("nPaginas", "255");
+		model.addAttribute("selecionado", "selected");
+		model.addAttribute("sinopse", "This is a book about JavaScript, programming, and the wonders of the digital. You can read it online here, or get your own paperback copy.\r\n" + 
+				"\r\n" + 
+				"Atualmente, estamos melhorando o que já está traduzido, focando na qualidade e precisão da tradução e entendimento do texto como um todo, além de tentar aplicar a gramática mais correta possível. Vários contribuidores ajudaram em diferentes partes do livro e, por isso, existem diversas oportunidades de melhorias.");
+		return "/livro-form";
+	}
+	
+	@GetMapping("/grupos")
+	public String grupos() {
+		return "/grupos";
+	}
+	
+	@GetMapping("/grupos/logado")
+	public String gruposLogado() {
+		return "/grupos";
+	}
+	
+	@GetMapping("/grupo/Nome-do-Grupo")
+	public String grupo() {
+		return "/grupo";
+	}
+	
+	@GetMapping("/grupo/Nome-do-Grupo/logado")
+	public String grupoLogado() {
+		return "/grupo";
+	}
+	
+	@GetMapping("/grupo/novo")
+	public String novoGrupo() {
+		return "grupo-form";
+	}
+	
+	@GetMapping("/grupo/editar")
+	public String novoGrupo(Model model) {
+		model.addAttribute("nome", "Nome do Grupo");
+		model.addAttribute("descricaoCurta", "Descrição do grupo, isto é uma descrição. Aqui tem coisas descritivas, que descrevem coisas. Descrevendo coisas.");
+		model.addAttribute("descricaoLonga", "Descrição 2, descrição mais extensa.\r\n" + 
+				"Aqui vai uma descrição mais extensa do grupo, pode conter regras e outras coisas.");
+		return "grupo-form";
 	}
 }
