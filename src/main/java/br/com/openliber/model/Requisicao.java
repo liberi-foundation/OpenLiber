75b64061b6b7f4c8b4ca750154d3d4f23f19619a
vinezof2 <joaovitorvinezof@gmail.com>
2019-07-31 19:58:06 -0300

    adicionando classe para requisições post e get

---
+++ src/main/java/br/com/openliber/model/Requisicao.java
@@ -0,0 +1,51 @@
+package br.com.openliber.model;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class Requisicao {
+	public static String post(String url, String dados) throws IOException {
+		// Iniciando requisição, definindo a URL e abrindo a conexão
+		URL obj = new URL(url);
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+		// Editando cabeçalho da requisição
+		con.setRequestMethod("POST");
+		con.setRequestProperty("Content-type", "application/x-www-form-urlencoded; charset=ISO-8859-1");
+
+		// Enviando a requisição via POST
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(dados);
+		wr.flush();
+		wr.close();
+
+		// Codigo da resposta
+		int responseCode = con.getResponseCode();
+
+		// Imprimindo código da resposta
+		System.out.println("Response Code : " + responseCode);
+
+		// Pegados dados da resposta
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		// Retornando resposta
+		return response.toString();
+	}
+
+	public static String get(String url) {
+		return null;
+	}
+}
