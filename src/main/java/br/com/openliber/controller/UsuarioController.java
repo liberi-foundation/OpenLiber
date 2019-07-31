7704e141a73f33de3e830f30f08523f9171325ed
jacilima <jacilenelima.contatos@gmail.com>
2019-07-30 21:26:21 -0300

    favoritar autor (avançços)

--- src/main/java/br/com/openliber/controller/UsuarioController.java
+++ src/main/java/br/com/openliber/controller/UsuarioController.java
@@ -5,7 +5,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
@@ -249,5 +252,15 @@ public class UsuarioController {
 		}
 
 		return retorno;
+	}
+
+	/*
+	 * Favoritar autor
+	 */
+	@PostMapping(value = "{/usuario/favoritar}", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String favoritarAutor(@RequestBody JSONObject data) {
+		String jsonRetorno = "{\"success\": true}";
+
+		return jsonRetorno;
 	}
 }
--- src/main/resources/static/js/favoritarAutor.js
+++ src/main/resources/static/js/favoritarAutor.js
@@ -1,5 +1,20 @@
-function favoritarAutor() {
-    btn = $('#btn-favoritar');
-    var textoAntigo = btn.html();
+function favoritarAutor(usuarioFavoritadoID, usuarioFavoritouID) {
+    var btn = $('#btn-favoritar');
     btn.attr('disabled', 'true').html('Carregando');
+
+    var dados = {
+        "usuarioFavoritadoID": usuarioFavoritadoID,
+        "usuarioFavoritouID": usuarioFavoritouID
+    };
+
+    $.ajax({
+        url: '/usuario/favoritar',
+        contentType: 'application/json; charset=utf-8',
+        type: 'POST',
+        data: JSON.stringify(dados),
+        success: function(data) {
+            btn.removeClass('btn-warning').addClass('btn-danger').attr('disabled', 'false');
+            console.log(data);
+        }
+    });
 }
