d537f3c7037bdeaecd26ea82987ce859a9b2c9a9
jacilima <jacilenelima.contatos@gmail.com>
2019-08-06 20:44:58 -0300

    Adicionando classe AcessoLivro e AcessoLivroDAO

---
+++ src/main/java/br/com/openliber/model/LivroAcesso.java
@@ -0,0 +1,62 @@
+package br.com.openliber.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.sun.istack.NotNull;
+
+@Entity
+public class LivroAcesso {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_livro_acesso")
+	private Integer id;
+
+	@ManyToOne
+	private Usuario usuario;
+
+	private LocalDateTime data;
+
+	@NotNull
+	@ManyToOne
+	private Livro livro;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+}
